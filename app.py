2af7f39cad8f862fce972a198c7536f13b8f89f5
Jonathan Juarez <jjuar2@uic.edu>
2021-12-08 01:40:42 -0600

    final push

--- app.py
+++ app.py
@@ -41,22 +41,4 @@ def start():
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 80, debug = True)
 
-# def get_dog_pic():
-
-# key = "AIzaSyA9lb2P7wmthZQCaIoVKEg3NiPsgdZNkhA"
-# url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key={key}"
-
-    # url = f"https://dog.ceo/api/breeds/image/random"
-    # req = requests.get(url)
-    # obj = req.json()
-    # dog_pic = obj
-    # print(dog_pic)
-
-# get_dog_pic()
-# payload={}
-# headers = {}
-
-# response = requests.request("GET", url, headers=headers, data=payload)
-
-# print(response.text)
 
